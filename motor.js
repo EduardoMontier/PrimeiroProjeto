305b4a5aee4ef24136c1a6c2a853a3e7d25a9763
EduardoMontier <paulomontier@live.com>
2021-08-19 19:48:35 -0300

    Add files via upload

---
+++ motor.js
@@ -0,0 +1,6 @@
+function carregar() {
+	var A = 10; // Estou salvando o valor 10 na variavel A
+	var B = "reais"; // Estou salvando o valor "reais" na variavel B
+	var C = A + B;	// Estou juntando as variaveis A e B na variavel C
+	alert(C); // Estou exibindo o valor da variavel C
+}
